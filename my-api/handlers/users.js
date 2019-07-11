dae9c5873998531f5423fc54e1fd6fa4fb1e17ef
JackWils91 <jackdavidwilson@gmail.com>
2019-07-11 08:08:12 +0100

    basic endpoints set up as an example spike

--- my-api/handlers/users.js
+++ my-api/handlers/users.js
@@ -1,166 +1,42 @@
 "use strict";
 
-const { password } = require("../config");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectID = require("mongodb").ObjectID;
-//const client = ;
-const uri = password;
-const dbName = "cluster0";
-const dbCollection = "users";
+// const { password } = require("../config");
+// const mongoose = require("mongoose");
+// const uriUtil = require("mongodb-uri");
+
+// const MongoClient = require("mongodb").MongoClient;
+// //const ObjectID = require("mongodb").ObjectID;
+// //const client = ;
+// const uri = password;
+// const dbName = "cluster0";
+// const dbCollection = "users";
 
 exports.list = async (event, context) => {
-  var dbClient;
-  try {
-    dbClient = await new MongoClient(uri, { useNewUrlParser: true });
-    if (!dbClient) throw new Error("Failed to connect to the database");
-    const dbUsers = dbClient.db(dbName).collection(dbCollection);
-
-    const users = await dbUsers.find().toArray();
-    dbClient.close();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(users)
-    };
-  } catch (err) {
-    if (dbClient) dbClient.close();
-    console.log(err);
-    return {
-      statusCode: 500,
-      body: err.message
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "this returns a list of users"
+    })
+  };
 };
 
 exports.get = async (event, context) => {
-  var dbClient;
-
-  if (!event || !event.pathParameters || !event.pathParameters.id) {
-    return { statusCode: 404, body: "" };
-  } else if (!ObjectID.isValid(event.pathParameters.id)) {
-    return { statusCode: 404, body: "" };
-  }
-
-  try {
-    dbClient = await MongoClient.connect(uri);
-    if (!dbClient) throw new Error("Failed to connect to the database");
-    const dbUsers = dbClient.db(dbName).collection(dbCollection);
-
-    const user = await dbUsers.findOne({
-      _id: ObjectID(event.pathParameters.id)
-    });
-
-    dbClient.close();
-    if (!user) {
-      return { statusCode: 404, body: "" };
-    } else {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(user)
-      };
-    }
-  } catch (err) {
-    if (dbClient) dbClient.close();
-    return {
-      statusCode: 500,
-      body: err.message
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "this returns an individual user"
+    })
+  };
 };
 
 exports.add = async (event, context) => {
-  var dbClient, body;
-
-  if (!event || !event.body) {
-    return { statusCode: 404, body: "" };
-  }
-
-  try {
-    body = JSON.parse(event.body);
-    dbClient = await MongoClient.connect(uri);
-    if (!dbClient) throw new Error("Failed to connect to the database");
-    const dbUsers = dbClient.db(dbName).collection(dbCollection);
-
-    const result = await dbUsers.insert(body);
-    dbClient.close();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ _id: result.insertedIds[0] })
-    };
-  } catch (err) {
-    if (dbClient) dbClient.close();
-    return {
-      statusCode: 500,
-      body: err.message
-    };
-  }
+  return { statusCode: 200, body: "this adds a user" };
 };
 
 exports.update = async (event, context) => {
-  var dbClient, body;
-  if (!event || !event.pathParameters || !event.pathParameters.id) {
-    return { statusCode: 404, body: "" };
-  } else if (!ObjectID.isValid(event.pathParameters.id)) {
-    return { statusCode: 404, body: "" };
-  }
-
-  try {
-    body = JSON.parse(event.body);
-    dbClient = await MongoClient.connect(uri);
-    if (!dbClient) throw new Error("Failed to connect to the database");
-    const dbUsers = dbClient.db(dbName).collection(dbCollection);
-
-    const result = await dbUsers.findOneAndUpdate(
-      { _id: ObjectID(event.pathParameters.id) },
-      { $set: body }
-    );
-    dbClient.close();
-    if (!result) {
-      return { statusCode: 404, body: "" };
-    } else {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ _id: result.value._id })
-      };
-    }
-  } catch (err) {
-    if (dbClient) dbClient.close();
-    return {
-      statusCode: 500,
-      body: err.message
-    };
-  }
+  return { statusCode: 200, body: "this updates a user" };
 };
 
 exports.remove = async (event, context) => {
-  var dbClient;
-  if (!event || !event.pathParameters || !event.pathParameters.id) {
-    return { statusCode: 404, body: "" };
-  } else if (!ObjectID.isValid(event.pathParameters.id)) {
-    return { statusCode: 404, body: "" };
-  }
-
-  try {
-    dbClient = await MongoClient.connect(uri);
-    if (!dbClient) throw new Error("Failed to connect to the database");
-    const dbUsers = dbClient.db(dbName).collection(dbCollection);
-
-    const result = await dbUsers.findOneAndDelete({
-      _id: ObjectID(event.pathParameters.id)
-    });
-    dbClient.close();
-    if (!result || !result.value) {
-      return { statusCode: 404, body: "" };
-    } else {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ _id: result.value._id })
-      };
-    }
-  } catch (err) {
-    if (dbClient) dbClient.close();
-    return {
-      statusCode: 500,
-      body: err.message
-    };
-  }
+  return { statusCode: 200, body: "this removes a user" };
 };
